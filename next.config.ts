a8415de1bbffbf1c9e96d33ef33eec5e9e6813f6
Avinash <avinashbaskar712009@gmail.com>
2026-08-06 14:08:08 +0530

    Update next.config.ts with image patterns and output
    Added image remote patterns and output configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,23 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: false,
+
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
   },
+
+
+  output: "standalone",
+
 };
 
 export default nextConfig;
-
