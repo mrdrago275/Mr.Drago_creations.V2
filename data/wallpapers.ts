7f66dc6f6e15378077b517229798f8c03c0e382d
Avinash <avinashbaskar712009@gmail.com>
2026-08-06 12:22:12 +0530

    Add wallpaper image paths to wallpapers.ts

---
+++ data/wallpapers.ts
@@ -0,0 +1,4 @@
+image: "/wallpapers/naruto/itachi-akatsuki.jpg"
+image: "/wallpapers/dragon-ball/goku-ultra-instinct.jpg"
+image: "/wallpapers/jujutsu-kaisen/gojo-satoru.jpg"
+image: "/wallpapers/demon-slayer/tanjiro-hinokami.jpg"
