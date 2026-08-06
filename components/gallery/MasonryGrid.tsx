c8e1ab4a80cde96be12e3852ddac455e6874e26d
Avinash <avinashbaskar712009@gmail.com>
2026-08-06 13:49:47 +0530

    Update MasonryGrid.tsx

--- components/gallery/MasonryGrid.tsx
+++ components/gallery/MasonryGrid.tsx
@@ -1,30 +1,79 @@
 'use client';
+
 import type { Wallpaper } from '../../data/wallpapers';
 import WallpaperItem from './WallpaperItem';
 
+
 export default function MasonryGrid({
+
   items,
   onOpen,
   onToggleLike,
   likes,
-}: {
-  items: Wallpaper[];
-  onOpen: (w: Wallpaper) => void;
-  onToggleLike: (id: string) => void;
-  likes: Record<string, boolean> | null | undefined;
-}) {
-  return (
+
+}:{
+
+  items:Wallpaper[];
+
+  onOpen:(w:Wallpaper)=>void;
+
+  onToggleLike:(id:string)=>void;
+
+  likes?:Record<string,boolean>;
+
+}){
+
+
+  if(!items.length){
+
+    return(
+      <div className="text-center text-silver py-20">
+        No wallpapers found
+      </div>
+    );
+
+  }
+
+
+
+  return(
+
     <div className="masonry">
-      {items.map((w) => (
-        <div key={w.id} className="masonry-item">
-          <WallpaperItem
-            wallpaper={w}
-            onOpen={() => onOpen(w)}
-            onToggleLike={() => onToggleLike(w.id)}
-            liked={!!(likes && likes[w.id])}
-          />
-        </div>
-      ))}
+
+      {
+        items.map((wallpaper)=>(
+
+          <div
+            key={wallpaper.id}
+            className="masonry-item"
+          >
+
+            <WallpaperItem
+
+              wallpaper={wallpaper}
+
+              onOpen={()=>
+                onOpen(wallpaper)
+              }
+
+              onToggleLike={()=>
+                onToggleLike(wallpaper.id)
+              }
+
+              liked={
+                !!likes?.[wallpaper.id]
+              }
+
+            />
+
+          </div>
+
+        ))
+      }
+
+
     </div>
+
   );
+
 }
