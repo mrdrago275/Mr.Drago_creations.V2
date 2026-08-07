06e30cb2b722414bdf67cd0fd06e0a6d8e8b1043
Avinash <avinashbaskar712009@gmail.com>
2026-08-07 13:15:01 +0530

    Update wallpapers.ts
    Fix wallpapers data

--- data/wallpapers.ts
+++ data/wallpapers.ts
@@ -1,20 +1,37 @@
-export const TRENDING=[
-  {  id: 1,
-     title: "Dragon Logo",
-     image: "/wallpapers/Dragon-Logo/dragon-logo.png"
-  },
-  ];
+export type Wallpaper = {
+  id: number;
+  title: string;
+  image: string;
+  author: string;
+  resolution: string;
+};
 
-export const FEATURED=[
-  {  id: 2,
-     title: "Dragon Logo",
-     image: "/wallpapers/Dragon-Logo/red-dragon-logo.png"
+export const TRENDING: Wallpaper[] = [
+  {
+    id: 1,
+    title: "Dragon Logo",
+    image: "/wallpapers/Dragon-Logo/dragon-logo.png",
+    author: "Mr. Drago",
+    resolution: "4K",
   },
-  ];
+];
 
-export const NEW_WALLPAPERS=[
-  {  id: 3,
-     title: "Dragon Logo",
-     image: "/wallpapers/Dragon-Logo/black-dragon.png"
+export const FEATURED: Wallpaper[] = [
+  {
+    id: 2,
+    title: "Red Dragon",
+    image: "/wallpapers/Dragon-Logo/red-dragon-logo.png",
+    author: "Mr. Drago",
+    resolution: "4K",
   },
-  ];
+];
+
+export const NEW_WALLPAPERS: Wallpaper[] = [
+  {
+    id: 3,
+    title: "Black Dragon",
+    image: "/wallpapers/Dragon-Logo/black-dragon.png",
+    author: "Mr. Drago",
+    resolution: "4K",
+  },
+];
