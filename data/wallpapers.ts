80a94092e3de1e96d57a92897d0371842b6fc19a
Avinash <avinashbaskar712009@gmail.com>
2026-08-07 13:04:15 +0530

    Fix image paths and rename NEW-WALLPAPER export
    Updated image paths to use absolute URLs and corrected the export name for new wallpapers.

--- data/wallpapers.ts
+++ data/wallpapers.ts
@@ -1,20 +1,20 @@
 export const TRENDING= [
   {  id: 1,
      title: "Dragon Logo",
-     image: "wallpapers/Dragon-Logo/dragon-logo.png"
+     image: "/wallpapers/Dragon-Logo/dragon-logo.png"
   },
   ];
 
 export const FEATURED= [
   {  id: 2,
      title: "Dragon Logo",
-     image: "wallpapers/Dragon-Logo/red-dragon-logo.png"
+     image: "/wallpapers/Dragon-Logo/red-dragon-logo.png"
   },
   ];
 
-export const NEW-WALLPAPER= [
+export const NEW-WALLPAPERS= [
   {  id: 3,
      title: "Dragon Logo",
-     image: " wallpapers/Dragon-Logo/black-dragon.png"
+     image: "/wallpapers/Dragon-Logo/black-dragon.png"
   },
   ];
