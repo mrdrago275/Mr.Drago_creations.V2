a0cf37322cc90ce1b5283ebd41d5aa16a1b5931b
Avinash <avinashbaskar712009@gmail.com>
2026-08-07 12:52:23 +0530

    Fix formatting and add spaces in TRENDING array

--- data/wallpapers.ts
+++ data/wallpapers.ts
@@ -1,6 +1,6 @@
-export const TRENDING=[
-  {  id:1,
-     title:Dragon-Logo,
-     image:"wallpapers/Dragon-Logo/dragon-logo.png"
+export const TRENDING= [
+  {  id: 1,
+     title: "Dragon Logo",
+     image: "wallpapers/Dragon-Logo/dragon-logo.png"
   },
   ];
